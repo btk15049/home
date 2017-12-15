0921a6cc9c7b719e3963d56fcccd729f1ded2e2a
btk15049 <btk15049@live.jp>
2017-12-15 13:58:17 +0900

    fix bug

--- pro/lib/RollHash.hpp
+++ pro/lib/RollHash.hpp
@@ -5,26 +5,31 @@ using namespace std;
 
 namespace roll{
     using H = unsigned long long;
-    constexpr H p[] = {1e9+9,1e9+7,90001};
-    constexpr int MX = 212345;
+                     //0123456789
+    constexpr H p[] = {1000000009,
+                       1000000007,
+                       90001};
+    constexpr int MX = 1123456;
     H hs[3][MX];
     H rh[3][MX];
-    template<int m>
+    template<int m = 1>
     void build(const string s){
         const int n = s.size();
         REP(i,m){
             hs[i][0] = 0;
             rh[i][0] = 1;
             REP(i,m)REP(j,n){
-                hs[j+1] += hs[j]*p[i] + s[j];
-                rh[j+1] =  rh[j]*p[i];
+                hs[i][j+1] = hs[i][j]*p[i] + s[j];
+                rh[i][j+1] = rh[i][j]*p[i];
             }
         }
     }
-    template<int m>
+    template<int m = 1>
     H get(const int l,const int r){
         H ret = 0;
-        REP(i,m)ret ^= hs[i][r] - hs[i][l] * rh[i][r-l];
+        REP(i,m){
+            ret ^= hs[i][r] - hs[i][l] * rh[i][r-l];
+        }
         return ret;
     }
 }
