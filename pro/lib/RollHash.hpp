e52ccaf6cd4c931244dbc6d355a11ec5f2df1807
btk <btk15049@live.jp>
2018-04-06 23:02:42 +0900

    文字コード修正&マクロ削除

--- pro/lib/RollHash.hpp
+++ pro/lib/RollHash.hpp
@@ -1,7 +1,5 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define FOR(i,bg,ed) for(int i=(bg);i<(ed);i++)
-#define REP(i,n) FOR(i,0,n)
 
 namespace roll{
     using H = unsigned long long;
@@ -15,19 +13,19 @@ namespace roll{
     template<int m = 1>
     void build(const string s){
         const int n = s.size();
-        REP(i,m){
+        for(int i = 0; i < m; i++){
             hs[i][0] = 0;
             rh[i][0] = 1;
-            REP(i,m)REP(j,n){
+        }
+        for(int i = 0; i < m; i++)for(int j = 0; j < n; j++){
                 hs[i][j+1] = hs[i][j]*p[i] + s[j];
                 rh[i][j+1] = rh[i][j]*p[i];
-            }
         }
     }
     template<int m = 1>
     H get(const int l,const int r){
         H ret = 0;
-        REP(i,m){
+        for(int i = 0; i < m; i++){
             ret ^= hs[i][r] - hs[i][l] * rh[i][r-l];
         }
         return ret;
