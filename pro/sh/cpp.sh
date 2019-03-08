3169cebab31e5ae79db076a08d0d27bc5e6249c8
btk <btk15049@live.jp>
2019-03-08 10:03:38 +0900

    update cpp.sh

--- pro/sh/cpp.sh
+++ pro/sh/cpp.sh
@@ -2,7 +2,7 @@ DIR1=`dirname $1`
 DIR2=`basename $1 .cpp`
 DIR="$DIR1/$DIR2"
 URL=`head -n 1 $1 | cut -c3-`
-if [ URL == ""]
+if [ $URL == "" ]
 then
     echo "URL is not found."
     exit 1
@@ -14,7 +14,7 @@ then
     mkdir $DIR
     oj dl $URL -d $DIR
 fi
-g++ -std=c++14 -Wall -Wextra $1
+g++ -std=c++14 -Wall -Wextra $1 -DBTK
 if [ -f a.out ]
 then
     oj test -d $DIR
