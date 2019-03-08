b94b474104a95ddcab80a269e1ccd4f20d7b7feb
btk <btk15049@live.jp>
2019-03-08 09:51:31 +0900

    update cpp.sh

--- pro/sh/cpp.sh
+++ pro/sh/cpp.sh
@@ -2,10 +2,15 @@ DIR1=`dirname $1`
 DIR2=`basename $1 .cpp`
 DIR="$DIR1/$DIR2"
 URL=`head -n 1 $1 | cut -c3-`
+if [ URL == ""]
+then
+    echo "URL is not found."
+    exit 1
+fi
 echo $DIR
 echo $URL
 if [ ! -d $DIR ]
-then 
+then
     mkdir $DIR
     oj dl $URL -d $DIR
 fi
