6911cf47add4a42b0dc2d2adff28e38b316ea617
btk <btk15049@live.jp>
2019-05-10 03:16:28 +0900

    refactor directory configue

--- pro/sh/cpp.sh
+++ pro/sh/cpp.sh
@@ -2,8 +2,9 @@
 #hoge.cpp -> hoge
 SUB=`basename ${1} .cpp`
 SU=`basename ${SUB} .test`
+SH_DIR=$(cd $(dirname $0); pwd)
 if [ "${SU}.test" == ${SUB} ]; then
-    ./sh/boost_test.sh ${1}
+    ${SH_DIR}/cpp.sh.d/boost_test.sh ${1}
 else
-    ./sh/runner.sh ${1}
+    ${SH_DIR}/cpp.sh.d/runner.sh ${1}
 fi
