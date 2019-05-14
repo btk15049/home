e8bf44b86c16efd7f96e97ab281b1d8784bc406f
btk <btk15049@live.jp>
2019-05-15 03:29:58 +0900

    big fix

---
+++ pro/sh/cpp/run_usual.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#第一引数にyaml 第二引数にほしいパラメータ名
+get_yaml_element() {
+    cat ${1} | yq .${2} | sed 's/"//g'
+}
+
+SH_DIR=$(cd $(dirname $0); pwd)
+
+if [ -e ${SH_DIR}/boost_test.sh.d/.build_args.yaml ]; then
+    ARGS=`get_yaml_element ${SH_DIR}/boost_test.sh.d/.build_args.yaml args`
+fi
+ARGS="${ARGS} -Wall -Wextra -std=c++17"
+
+
+echo ""
+echo "<build>"
+${SH_DIR}/build.sh ${1} ${ARGS}
+
+#コンパイルの成功チェック
+if [ ! -f a.out ]; then
+    echo "a.out is not found."
+    exit 1
+fi
+
+echo ""
+echo "<run>"
+
+#run
+./a.out
+rm a.out
