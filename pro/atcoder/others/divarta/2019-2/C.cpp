f6ddb7b03aa114594aa5f9c6ffb13d2a0644d1ba
btk <btk15049@live.jp>
2019-06-18 22:38:42 +0900

    diverta2

---
+++ pro/atcoder/others/divarta/2019-2/C.cpp
@@ -0,0 +1,192 @@
+// https://atcoder.jp/contests/diverta2019-2/tasks/diverta2019_2_c
+/*<head>*/
+// #include "Template.hpp"
+/*</head>*/
+
+/*<body>*/
+/* #region header */
+/* #region 1*/
+/**
+ * @file Template.hpp
+ * @brief 競技プログラミング用テンプレート
+ * @author btk15049
+ * @date 2019/05/02
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+/* #region macro */
+#ifdef BTK
+#    define DEBUG if (1)
+#    define CIN_ONLY if (0)
+#else
+#    define DEBUG if (0)
+#    define CIN_ONLY if (1)
+#endif
+/** @def
+ * ALLマクロ
+ */
+#define ALL(v) (v).begin(), (v).end()
+
+/** @def
+ * 再帰ラムダをするためのマクロ
+ */
+#define REC(ret, ...) std::function<ret(__VA_ARGS__)>
+/* #endregion */
+
+namespace _Template_ {
+    /**
+     * @brief cin高速化処理を行うための構造体
+     * @details CIN_ONLYマクロで動作が変わる
+     */
+    struct cww {
+        cww() {
+            CIN_ONLY {
+                ios::sync_with_stdio(false);
+                cin.tie(0);
+            }
+        }
+    } star;
+    /**
+     * @brief change min
+     * @tparam T 型
+     * @param l 参照
+     * @param r 非参照
+     * @return 更新があればtrue
+     */
+    template <typename T>
+    inline bool chmin(T& l, T r) {
+        bool a = l > r;
+        if (a) l = r;
+        return a;
+    }
+    /**
+     * @brief chminのmax版
+     * @see chmin
+     */
+    template <typename T>
+    inline bool chmax(T& l, T r) {
+        bool a = l < r;
+        if (a) l = r;
+        return a;
+    }
+    /**
+     * @brief
+     * vectorに直接cin流すためのやつ
+     * @tparam T
+     * @param is
+     * @param v
+     * @return istream&
+     */
+    template <typename T>
+    istream& operator>>(istream& is, vector<T>& v) {
+        for (auto& it : v) is >> it;
+        return is;
+    }
+    /**
+     * @brief
+     * rangeを逆向きに操作したいとき用
+     * @details
+     * ループの範囲は[bg,ed)なので注意
+     * @see range
+     */
+    class reverse_range {
+      private:
+        struct I {
+            int x;
+            int operator*() { return x - 1; }
+            bool operator!=(I& lhs) { return x > lhs.x; }
+            void operator++() { --x; }
+        };
+        I i, n;
+
+      public:
+        reverse_range(int n) : i({0}), n({n}) {}
+        reverse_range(int i, int n) : i({i}), n({n}) {}
+        I& begin() { return n; }
+        I& end() { return i; }
+    };
+    /**
+     * @brief
+     * python みたいな range-based for を実現
+     * @details
+     * ループの範囲は[bg,ed)なので注意
+     * !つけると逆向きにループが回る (reverse_range)
+     * 空間計算量はO(1)
+     * 使わない変数ができて警告が出がちなので，unused_varとかを使って警告消しするとよい
+     */
+    class range {
+      private:
+        struct I {
+            int x;
+            int operator*() { return x; }
+            bool operator!=(I& lhs) { return x < lhs.x; }
+            void operator++() { ++x; }
+        };
+        I i, n;
+
+      public:
+        range(int n) : i({0}), n({n}) {}
+        range(int i, int n) : i({i}), n({n}) {}
+        I& begin() { return i; }
+        I& end() { return n; }
+        reverse_range operator!() { return reverse_range(*i, *n); }
+    };
+    /**
+     * @brief
+     * rangeで生まれる使わない変数を消す用（警告消し）
+     */
+    template <typename T>
+    inline T& unused_var(T& v) {
+        return v;
+    }
+    using LL = long long;
+} // namespace _Template_
+using namespace _Template_;
+/* #endregion */
+/* #endregion */
+/*</body>*/
+
+LL A[212345];
+int main() {
+    int N;
+    cin >> N;
+    for (int i : range(N)) cin >> A[i];
+    vector<LL> p, n, z;
+    sort(A, A + N);
+    for (int i : range(N)) {
+        if (A[i] < 0)
+            n.push_back(A[i]);
+        else
+            p.push_back(A[i]);
+    }
+    reverse(ALL(p));
+    if (n.size() == 0) {
+        n.push_back(p.back());
+        p.pop_back();
+    }
+    if (p.size() == 0) {
+        p.push_back(n.back());
+        n.pop_back();
+    }
+    vector<LL> a, b;
+    while (p.size() > 1) {
+        a.push_back(n.back());
+        b.push_back(p.back());
+        n.back() -= p.back();
+        p.pop_back();
+    }
+    while (n.size()) {
+        a.push_back(p.back());
+        b.push_back(n.back());
+        p.back() -= n.back();
+        n.pop_back();
+    }
+    cout << p.back() << "\n";
+    for (int i : range(N - 1)) {
+        cout << a[i] << " " << b[i] << "\n";
+    }
+
+
+    return 0;
+}
---
+++ pro/atcoder/others/divarta/2019-2/D.cpp
@@ -0,0 +1,181 @@
+// https://atcoder.jp/contests/diverta2019-2/tasks/diverta2019_2_d
+/*<head>*/
+// #include "Template.hpp"
+/*</head>*/
+
+/*<body>*/
+/* #region header */
+/* #region 1*/
+/**
+ * @file Template.hpp
+ * @brief 競技プログラミング用テンプレート
+ * @author btk15049
+ * @date 2019/05/02
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+/* #region macro */
+#ifdef BTK
+#    define DEBUG if (1)
+#    define CIN_ONLY if (0)
+#else
+#    define DEBUG if (0)
+#    define CIN_ONLY if (1)
+#endif
+/** @def
+ * ALLマクロ
+ */
+#define ALL(v) (v).begin(), (v).end()
+
+/** @def
+ * 再帰ラムダをするためのマクロ
+ */
+#define REC(ret, ...) std::function<ret(__VA_ARGS__)>
+/* #endregion */
+
+namespace _Template_ {
+    /**
+     * @brief cin高速化処理を行うための構造体
+     * @details CIN_ONLYマクロで動作が変わる
+     */
+    struct cww {
+        cww() {
+            CIN_ONLY {
+                ios::sync_with_stdio(false);
+                cin.tie(0);
+            }
+        }
+    } star;
+    /**
+     * @brief change min
+     * @tparam T 型
+     * @param l 参照
+     * @param r 非参照
+     * @return 更新があればtrue
+     */
+    template <typename T>
+    inline bool chmin(T& l, T r) {
+        bool a = l > r;
+        if (a) l = r;
+        return a;
+    }
+    /**
+     * @brief chminのmax版
+     * @see chmin
+     */
+    template <typename T>
+    inline bool chmax(T& l, T r) {
+        bool a = l < r;
+        if (a) l = r;
+        return a;
+    }
+    /**
+     * @brief
+     * vectorに直接cin流すためのやつ
+     * @tparam T
+     * @param is
+     * @param v
+     * @return istream&
+     */
+    template <typename T>
+    istream& operator>>(istream& is, vector<T>& v) {
+        for (auto& it : v) is >> it;
+        return is;
+    }
+    /**
+     * @brief
+     * rangeを逆向きに操作したいとき用
+     * @details
+     * ループの範囲は[bg,ed)なので注意
+     * @see range
+     */
+    class reverse_range {
+      private:
+        struct I {
+            int x;
+            int operator*() { return x - 1; }
+            bool operator!=(I& lhs) { return x > lhs.x; }
+            void operator++() { --x; }
+        };
+        I i, n;
+
+      public:
+        reverse_range(int n) : i({0}), n({n}) {}
+        reverse_range(int i, int n) : i({i}), n({n}) {}
+        I& begin() { return n; }
+        I& end() { return i; }
+    };
+    /**
+     * @brief
+     * python みたいな range-based for を実現
+     * @details
+     * ループの範囲は[bg,ed)なので注意
+     * !つけると逆向きにループが回る (reverse_range)
+     * 空間計算量はO(1)
+     * 使わない変数ができて警告が出がちなので，unused_varとかを使って警告消しするとよい
+     */
+    class range {
+      private:
+        struct I {
+            int x;
+            int operator*() { return x; }
+            bool operator!=(I& lhs) { return x < lhs.x; }
+            void operator++() { ++x; }
+        };
+        I i, n;
+
+      public:
+        range(int n) : i({0}), n({n}) {}
+        range(int i, int n) : i({i}), n({n}) {}
+        I& begin() { return i; }
+        I& end() { return n; }
+        reverse_range operator!() { return reverse_range(*i, *n); }
+    };
+    /**
+     * @brief
+     * rangeで生まれる使わない変数を消す用（警告消し）
+     */
+    template <typename T>
+    inline T& unused_var(T& v) {
+        return v;
+    }
+    using LL = long long;
+} // namespace _Template_
+using namespace _Template_;
+/* #endregion */
+/* #endregion */
+/*</body>*/
+
+constexpr int sz = 5123 * 5123;
+LL dp[sz];
+int main() {
+    LL N;
+    cin >> N;
+    vector<LL> A(3), B(3);
+    cin >> A >> B;
+    {
+        for (int i : range(3)) {
+            for (int j : range(A[i], N + 1)) {
+                chmax(dp[j], dp[j - A[i]] + B[i]);
+            }
+        }
+        LL NN = N;
+        for (int i : range(N + 1)) chmax(NN, dp[i] + (N - i));
+        N = NN;
+    }
+    swap(A, B);
+    fill(dp, dp + N + 1, 0ll);
+    {
+        for (int i : range(3)) {
+            for (int j : range(A[i], N + 1)) {
+                chmax(dp[j], dp[j - A[i]] + B[i]);
+            }
+        }
+        LL NN = N;
+        for (int i : range(N + 1)) chmax(NN, dp[i] + (N - i));
+        N = NN;
+    }
+    cout << N << endl;
+    return 0;
+}
